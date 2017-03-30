09f130975a74c4755210850df4c087c17e60bf6a
Matt Rae <matt@chalk.com>
2017-03-30 16:53:10 -0400

    Testing design chats site through this project

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function($) {
+    // $('.price-items .entry').each(function(index, el) {
+    //    if( $(this).hasClass('current') ){
+    //         $('.price-value').val($(this).data('value'));
+    //    }
+    // });
+    // $('.price-items .entry').each(function(index, el) {
+    //    if( $(this).hasClass('current') ){
+    //         $('.price-display').html($(this).data('price'));
+    //    }
+    // });
+    // $('.price-items .entry').click(function(event) {
+    //     $('.price-items .entry').removeClass('current');
+    //     $(this).addClass('current');
+    //     $('.price-display').html($(this).data('price'));
+    //     $('.price-value').val($(this).data('value'));
+    // });
+});
